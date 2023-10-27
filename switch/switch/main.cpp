1d0fba796c2f363247ecab87ed58360de59ab9f0
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-10-27 22:41:11 +0800

    Add files via upload

---
+++ switch/switch/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  switch
+//
+//  Created by Nisha Ramprasath on 27/10/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int x=9;
+    
+    switch(x){
+        case 1:cout<<"One";
+            break;
+        case 2:cout<<"two";
+            break;
+        case 3:cout<<"tree";
+        break;
+        default:cout<<"invalif number";
+    }
+
+    return 0;
+}
